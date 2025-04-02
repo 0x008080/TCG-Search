7300ec540992df74b3e898f8e62bead6f031c5d8
0x008080 <caperez.0013@gmail.com>
2025-04-02 13:53:18 -0700

    add outlines for Search component

---
+++ src/components/Search.tsx
@@ -0,0 +1,19 @@
+
+
+function Search () {
+    return (
+        <div className="row">
+            <div className="column">
+                <h2 itemID="pokemon-name">
+                    Furret
+                </h2>
+                <img className="pokemon-img" itemID="pokemon-img" src="src/assets/test.jpg"></img>
+            </div>
+            <div className="column">
+                <h2 itemID="pokemon-list">test</h2>
+            </div>
+        </div>
+    )
+}
+
+export default Search;
---
+++ src/components/Title.tsx
@@ -0,0 +1,10 @@
+
+function Title() {
+    return (
+        <h1>
+            Pokémon TCG Search
+        </h1>
+    )
+}
+
+export default Title;
